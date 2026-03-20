e60035c7d738a14f47747c8dea20ff8500f64792
thanhson003 <nthanhson0404@gmail.com>
2026-03-20 10:17:14 +0700

    10:15

---
+++ geometric/src/Test.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        ComparableCircle[] circles = new ComparableCircle[3];
+        circles[0] = new ComparableCircle(3.6);
+        circles[1] = new ComparableCircle();
+        circles[2] = new ComparableCircle(3.5, "indigo", false);
+
+        System.out.println("Pre-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+
+        Arrays.sort(circles);
+
+        System.out.println("After-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+    }
+}
+
+/*
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class CircleComparatorTest {
+    public static void main(String[] args) {
+        Circle[] circles = new Circle[3];
+        circles[0] = new Circle(3.6);
+        circles[1] = new Circle();
+        circles[2] = new Circle(3.5, "indigo", false);
+
+        System.out.println("Pre-sorted:");
+        for (Circle circle : circles) {
+            System.out.println(circle);
+        }
+
+        Comparator circleComparator = new CircleComparator();
+        Arrays.sort(circles, circleComparator);
+
+        System.out.println("After-sorted:");
+        for (Circle circle : circles) {
+            System.out.println(circle);
+        }
+    }
+} */
